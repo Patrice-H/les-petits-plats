3b95dfe2ada2cdcb1ad8aa516fd4d3e48e4753f6
Patrice-H <https://github.com/>
2022-02-10 17:47:08 +0100

    Add main search functionality

--- scripts/functionalities/search.js
+++ scripts/functionalities/search.js
@@ -1,4 +1,125 @@
-function getIngredients(recipes) {
+function getRecipesByUnion(elements) {
+    let recipes = getRecipesFromElement(elements[0].type, elements[0].name);
+    for (let i = 1; i < elements.length; i++) {
+        let temp = getRecipesFromElement(elements[i].type, elements[i].name);
+        for (let j = 0; j < temp.length; j++) {
+            let control = true;
+            for (let k = 0; k < recipes.length; k++) {
+                if (recipes[k].id === temp[j].id) {
+                    control = false;
+                } 
+            }
+            if(control) {
+                recipes[recipes.length] = temp[j];
+            }
+        }
+    }
+
+    return recipes;
+}
+
+function getRecipesByIntersect(elements) {
+    let recipes = getRecipesFromElement(elements[0].type, elements[0].name); 
+    for (let i = 1; i < elements.length; i++) {
+        let temp = [];
+        let tab = getRecipesFromElement(elements[i].type, elements[i].name);
+        for (let j = 0; j < recipes.length; j++) {
+            for (let k = 0; k < tab.length; k++) {
+                if (tab[k].id === recipes[j].id) {
+                    temp[temp.length] = tab[k];
+                } 
+            }
+        }
+        recipes = temp;
+    }
+    
+    return recipes;
+}
+
+function getRecipesFromTitle(title) {
+    let recipes = [];
+    for (let i = 0; i < RECIPES.length; i++) {
+        if (RECIPES[i].name.toLowerCase() === title.toLowerCase()) {
+            recipes[recipes.length] = RECIPES[i];
+        };
+    }
+
+    return recipes;
+}
+
+function getRecipesFromDescription(description) {
+    let recipes = [];
+    for (let i = 0; i < RECIPES.length; i++) {
+        if (RECIPES[i].description.toLowerCase() === description.toLowerCase()) {
+            recipes[recipes.length] = RECIPES[i];
+        };
+    }
+
+    return recipes;
+}
+
+function getRecipesFromIngredient(element) {
+    let recipes = [];
+    for (let i = 0; i < RECIPES.length; i++) {
+        for (let j = 0; j < RECIPES[i].ingredients.length; j++) {
+            if (RECIPES[i].ingredients[j].ingredient.toLowerCase() === element.toLowerCase()) {
+                recipes[recipes.length] = RECIPES[i];
+            }
+        }
+    }
+    
+    return recipes;
+}
+
+function getRecipesFromAppliance(element) {
+    let recipes = [];
+    for (let i = 0; i < RECIPES.length; i++) {
+        if (RECIPES[i].appliance.toLowerCase() === element.toLowerCase()) {
+            recipes[recipes.length] = RECIPES[i];
+        }
+    }
+
+    return recipes;
+}
+
+function getRecipesFromUstensil(element) {
+    let recipes = [];
+    for (let i = 0; i < RECIPES.length; i++) {
+        for (let j = 0; j < RECIPES[i].ustensils.length; j++) {
+            if (RECIPES[i].ustensils[j].toLowerCase() === element.toLowerCase()) {
+                recipes[recipes.length] = RECIPES[i];
+            }
+        }
+    }
+
+    return recipes;
+}
+
+function getRecipesFromElement(type, element) {
+    let recipes;
+    switch (type) {
+        case 'ingredients':
+            recipes = getRecipesFromIngredient(element);
+            break;
+        case 'appliance':
+            recipes = getRecipesFromAppliance(element);
+            break;
+        case 'ustensils':
+            recipes = getRecipesFromUstensil(element);
+            break
+        case 'title':
+            recipes = getRecipesFromTitle(element);
+            break;
+        case 'description':
+            recipes = getRecipesFromDescription(element);
+        default:
+            break;
+    }
+
+    return recipes;
+}
+
+function getIngredientsFromRecipes(recipes) {
     let ingredientsMenu = [];
     for (let i = 0; i < recipes.length; i++) {
         for (let j = 0; j < recipes[i].ingredients.length; j++) {
@@ -17,7 +138,7 @@ function getIngredients(recipes) {
     return ingredientsMenu;
 }
 
-function getAppliance(recipes) {
+function getApplianceFromRecipes(recipes) {
     let applianceMenu = [];
     for (let i = 0; i < recipes.length; i++) {
         let single = true;
@@ -34,7 +155,7 @@ function getAppliance(recipes) {
     return applianceMenu;
 }
 
-function getUstensils(recipes) {
+function getUstensilsFromRecipes(recipes) {
     let ustensilsMenu = [];
     for (let i = 0; i < recipes.length; i++) {
         for (let j = 0; j < recipes[i].ustensils.length; j++) {
@@ -51,4 +172,114 @@ function getUstensils(recipes) {
     }
 
     return ustensilsMenu;
+}
+
+function getTitlesFromResearch(wordpart) {
+    let titleTable = [];
+    for (let i = 0; i < RECIPES.length; i++) {
+        if (RECIPES[i].name.toLowerCase().includes(wordpart.toLowerCase())) {
+            titleTable[titleTable.length] = {
+                'type': 'title',
+                'name': RECIPES[i].name
+            }
+        }
+    }
+
+    return titleTable;
+}
+
+function getDescriptionsFromResearch(wordpart) {
+    let descriptionTable = [];
+    for (let i = 0; i < RECIPES.length; i++) {
+        if (RECIPES[i].description.toLowerCase().includes(wordpart.toLowerCase())) {
+            descriptionTable[descriptionTable.length] = {
+                'type': 'description',
+                'name': RECIPES[i].description
+            }
+        }
+    }
+
+    return descriptionTable;
+}
+
+function getIngredientsFromResearch(wordpart) {
+    let allIngredients = getIngredientsFromRecipes(RECIPES);
+    let ingredientsTable = [];
+    for (let i = 0; i < allIngredients.length; i++) {
+        if (allIngredients[i].toLowerCase().includes(wordpart.toLowerCase())) {
+            ingredientsTable[ingredientsTable.length] = {
+                'type': 'ingredients',
+                'name': allIngredients[i]
+            };
+        }
+    }
+
+    return ingredientsTable;
+}
+
+function getApplianceFromResearch(wordpart) {
+    let allAppliance = getApplianceFromRecipes(RECIPES);
+    let applianceTable = [];
+    for (let i = 0; i < allAppliance.length; i++) {
+        if (allAppliance[i].toLowerCase().includes(wordpart.toLowerCase())) {
+            applianceTable[applianceTable.length] = {
+                'type': 'appliance',
+                'name': allAppliance[i]
+            };
+        } 
+    }
+
+    return applianceTable;
+}
+
+function getUstensilsFromResearch(wordpart) {
+    let allUstensils = getUstensilsFromRecipes(RECIPES);
+    let ustensilsTable = [];
+    for (let i = 0; i < allUstensils.length; i++) {
+        if (allUstensils[i].toLowerCase().includes(wordpart.toLowerCase())) {
+            ustensilsTable[ustensilsTable.length] = {
+                'type': 'ustensils',
+                'name': allUstensils[i]
+            };
+        }
+    }
+
+    return ustensilsTable;
+}
+
+function getElementsFromResearch(type, wordpart) {
+    let elements;
+    switch (type) {
+        case 'ingredients':
+            elements = getIngredientsFromResearch(wordpart);
+            break;
+        case 'appliance':
+            elements = getApplianceFromResearch(wordpart);
+            break;
+        case 'ustensils':
+            elements = getUstensilsFromResearch(wordpart);
+            break;
+        default:
+            break;
+    }
+
+    return elements
+}
+
+function getElementsFromMainResearch(wordpart) {
+    let elements = [];
+    let title = getTitlesFromResearch(wordpart);
+    let description = getDescriptionsFromResearch(wordpart);
+    let ingredients = getIngredientsFromResearch(wordpart);
+    for (let i = 0; i < title.length; i++) {
+        elements[elements.length] = title[i]; 
+    }
+    for (let j = 0; j < description.length; j++) {
+        elements[elements.length] = description[j];
+    }
+    for (let k = 0; k < ingredients.length; k++) {
+        elements[elements.length] = ingredients[k];
+    }
+
+    return elements;
 }
--- scripts/views/thematic_menus.js
+++ scripts/views/thematic_menus.js
@@ -1,18 +1,38 @@
-function closeTag(element) {
-    const TAG_BAR = document.getElementById('thematic-tag-bar');
-    const ELEMENT = document.getElementById(element);
-    TAG_BAR.removeChild(ELEMENT);
+function getFilterTags() {
+    let nodes = TAG_BAR.childNodes;
+    let elements = [];
+    nodes.forEach(node => {
+        let type = node.id.split('-')[0];
+        let name = node.id.split('-')[1];
+        let element = {
+            'type': type,
+            'name': name
+        }
+        elements.push(element);
+    });
+
+    return elements;
 }
 
-function displayTag(tagkind, tagname) {
-    const TAG_BAR = document.getElementById('thematic-tag-bar');
-    let tagClass = tagkind.split('-')[0] + '-tag';
+function closeTag(element) {
+    const ELEMENT = document.getElementById(element);
+    TAG_BAR.removeChild(ELEMENT);
+    let recipes = RECIPES;
+    if (TAG_BAR.firstChild !== null) {
+        let elements = getFilterTags();
+        recipes = getRecipesByIntersect(elements);
+    }
+    updateContent(recipes);
+}
+
+function displayTag(type, name) {
+    let tagClass = type + '-tag';
     const TAG_ELEMENT = document.createElement('p');
     const TAG = document.createElement('span');
     const TAG_BUTTON = document.createElement('span');
     TAG_ELEMENT.setAttribute('class', tagClass);
-    TAG_ELEMENT.setAttribute('id', tagkind.split('-')[0] + '-' + tagname);
-    TAG.textContent = tagname;
+    TAG_ELEMENT.setAttribute('id', type + '-' + name);
+    TAG.textContent = name;
     TAG_BUTTON.setAttribute('class', 'far fa-times-circle');
     TAG_BUTTON.addEventListener('click', () => {
         closeTag(TAG_ELEMENT.id);
@@ -22,24 +42,95 @@ function displayTag(tagkind, tagname) {
     TAG_BAR.appendChild(TAG_ELEMENT);
 }
 
-function buildMenus(recipes) {
+function manageMainSearch(wordpart) {
+    let elements = getElementsFromMainResearch(wordpart);
+    let recipes = RECIPES;
+    if (wordpart.length >= 3) {
+        recipes = getRecipesByUnion(elements);
+    }
+    updateContent(recipes);
+}
+
+function manageDisplayFromInputMenuResearch(type, wordpart) {
+    let elements;
+    let recipes = RECIPES;
+    if (wordpart.length >= 3) {
+        elements = getElementsFromResearch(type, wordpart);
+        if (elements.length > 0) {
+            recipes = getRecipesByUnion(elements);
+            updateContent(recipes);
+            buildMenuFromInputSearch(elements);
+        } else {
+            buildMenuFromRecipes(recipes)
+            resetChildNodes(document.getElementById(type + '-content'));
+        }
+    } else {
+        updateContent(recipes);
+    }
+}
+
+function manageDisplayFromTagResearch(filter, element) {
+    displayTag(filter, element);
+    let elements = getFilterTags();
+    let recipes = getRecipesByIntersect(elements);   
+    updateContent(recipes);
+    closeThematicMenu(filter);
+}
+
+function updateContent(recipes) {
+    buildRecipesGallery(recipes);
+    buildMenuFromRecipes(recipes);
+}
+
+function resetChildNodes(domelement) {
+    while(domelement.hasChildNodes()) {
+        domelement.removeChild(domelement.firstChild);
+    }
+}
+
+function buildMenuFromInputSearch(elements) {
+    const MENU = document.getElementById(elements[0].type + '-content');
+    let tagClass = elements[0].type + '-choice';
+    resetChildNodes(MENU);
+    const UL = document.createElement('ul');
+    const LOOP = 3 - (elements.length % 3);
+    for (let i = 0; i < elements.length; i++) {
+        let li = document.createElement('li');
+        li.setAttribute('class', tagClass);
+        li.setAttribute('id', elements[i].name);
+        li.textContent = elements[i].name;
+        li.addEventListener('click', () => {
+            manageDisplayFromTagResearch(li.className.split('-')[0], li.id);
+        });
+        UL.appendChild(li);
+    }
+    for (let i = 0; i < LOOP; i++) {
+        let li = document.createElement('li');
+        li.setAttribute('class', 'blank');
+        UL.appendChild(li);
+    }
+    MENU.appendChild(UL);
+}
+
+function buildMenuFromRecipes(recipes) {
     const MENUS = ['ingredients-content', 'appliance-content', 'ustensils-content'];
     let elements;
     let tagClass;
     MENUS.forEach(menu => {
         const CONTENT = document.getElementById(menu);
+        resetChildNodes(CONTENT);
         const UL = document.createElement('ul');
         switch (menu) {
             case 'ingredients-content':
-                elements = getIngredients(recipes);
+                elements = getIngredientsFromRecipes(recipes);
                 tagClass = 'ingredients-choice'
                 break;
             case 'appliance-content':
-                elements = getAppliance(recipes);
+                elements = getApplianceFromRecipes(recipes);
                 tagClass = 'appliance-choice'
                 break;
             case 'ustensils-content':
-                elements = getUstensils(recipes);
+                elements = getUstensilsFromRecipes(recipes);
                 tagClass = 'ustensils-choice'
                 break;
             default:
@@ -52,7 +143,7 @@ function buildMenus(recipes) {
             li.setAttribute('id', elements[i]);
             li.textContent = elements[i];
             li.addEventListener('click', () => {
-                displayTag(li.className, li.id);
+                manageDisplayFromTagResearch(li.className.split('-')[0], li.id);
             });
             UL.appendChild(li);
         }
@@ -85,8 +176,11 @@ function openThematicMenu(name) {
 
 const THEMATIC_BUTTONS = Array.from(document.getElementsByClassName('thematic-search-button'));
 const CLOSE_THEMATIC_MENU_BUTTONS = Array.from(document.getElementsByClassName('close-btn'));
+const THEMATIC_INPUTS = Array.from(document.getElementsByClassName('thematic-input'));
+const TAG_BAR = document.getElementById('thematic-tag-bar');
+const MAIN_INPUT = document.getElementById('main-search');
 
-buildMenus(RECIPES);
+buildMenuFromRecipes(RECIPES);
 
 THEMATIC_BUTTONS.forEach(button => {
     button.addEventListener('click', () => {
@@ -97,4 +191,12 @@ CLOSE_THEMATIC_MENU_BUTTONS.forEach(button => {
     button.addEventListener('click', () => {
         closeThematicMenu(button.id.split('-')[0]);
     });
+});
+THEMATIC_INPUTS.forEach(input => {
+    input.addEventListener('input', (evt) => {
+        manageDisplayFromInputMenuResearch(input.id.split('-')[0], evt.target.value);
+    });
+});
+MAIN_INPUT.addEventListener('input', (evt) => {
+    manageMainSearch(evt.target.value);
 });
